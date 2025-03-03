edc7fb6cc6caa951b9db2b27baf48cbc5626fde3
trungnguyenvn22 <demodecoding@gmail.com>
2025-03-03 17:38:09 +0700

    PrefixSum- ex5

--- src/prefix_sum/PrefixSum.java
+++ src/prefix_sum/PrefixSum.java
@@ -4,6 +4,53 @@ import java.util.Scanner;
 
 public class PrefixSum {
 
+//    Cho mảng số nguyên A[] gồm N phần tử, có Q thao tác, mỗi thao tác yêu cầu các
+//    bạn tăng tất cả các phần tử từ chỉ số L tới chỉ số R lên K đơn vị. Bạn hãy in ra
+//    mảng sau khi thực hiện các thao tác trên.
+//            Input Format
+// Dòng 1 là N và Q
+// Dòng 2 là N phần tử trong mảng A[]
+// Q dòng tiếp theo mỗi dòng là 3 số L, R, K
+//    Constraints
+// 1<=N<=10^6
+//             1<=Q<=10^5
+//             0<=L<=R<=N-1, 0<=A[i],K<=1000
+//Output Format
+//    In ra mảng sau Q thao tác
+//    Sample Input 0
+//            7 3
+//            8 5 8 9 7 6 9
+//            0 5 0
+//            0 5 0
+//            1 5 1
+//    Sample Output 0
+//            8 6 9 10 8 7 9
+    static void updateOperation(){
+        Scanner sc = new Scanner(System.in);
+        int length = sc.nextInt();
+        int [] arr =  new int [length];
+        int q = sc.nextInt();
+        System.out.println("Enter elements value: ");
+        for (int i = 0; i< length; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Enter Left & right $ k value: ");
+        while (q > 0){
+
+            int l = sc.nextInt();
+            int r = sc.nextInt();
+            int k = sc.nextInt();
+
+            for(int i = l; i<=r; i++){
+                arr[i] += k;
+            }
+            q--;
+        }
+        for(int item: arr){
+            System.out.print(item + " - ");
+        }
+
+    }
     public static void main(String[] args) {
 
         boolean check = true;
@@ -37,6 +84,9 @@ public class PrefixSum {
 
                 }
                 case 5:{
+                    System.out.println("Update Operation: ");
+                    updateOperation();
+                    break;
 
                 }
                 case 6:{
